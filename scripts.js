15209facc0b0b382099d3549179770919a4633ea
Tajeka <johnsontajeka@gmail.com>
2023-11-05 21:06:57 -0500

    Edited scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,21 @@
 /* Add your JavaScript to this file */
+
+window.addEventListener("load", (event) => {
+    const Form= document.querySelector("form");
+    const msg= document.querySelector(".message");
+
+    Form.addEventListener("submit", function(e){
+        e.preventDefault();
+
+        const emailadd= document.getElementById("email").value; 
+        
+        if (emailadd !== "") {
+            msg.textContent = "Thank you! Your email address" + emailadd + "has been added to our mailing list!";
+
+            emailInput.value = "";
+        }
+        else {
+            msg.textContent = "Please enter a valid email address.";
+        }
+    });
+});
